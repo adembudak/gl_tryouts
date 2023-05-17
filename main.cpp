2e0677ac309016582666da52ee5c5c8359aeda10
adem <adembudak@outlook.com>
2023-05-17 13:05:16 +0300

    Some initialization reorder
    Use C stdio for console out

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <GL/freeglut.h>
 #include <GL/gl.h>
 
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 
 void handleKeyboard(unsigned char key, int x, int y) {
@@ -11,11 +13,11 @@ void handleKeyboard(unsigned char key, int x, int y) {
 }
 
 void handleMouse(int btn, int state, int x, int y) {
-  std::cout << ((state == GLUT_DOWN) ? "down " : "up   ") << x << ' ' << y << '\n';
+  std::printf("%s %d %d\n", ((state == GLUT_DOWN) ? "down " : "up   "), x, y);
 }
 
 void handleReshape(int new_pos_w, int new_pos_h) { //
-  std::cout << "Reshaped to: " << new_pos_w << ' ' << new_pos_h << '\n';
+  std::printf("Reshaped to: %d %d\n", new_pos_w, new_pos_h);
   glViewport(0, 0, new_pos_w, new_pos_h);
   glutPostRedisplay();
 }
@@ -26,17 +28,21 @@ void handleRendering() {
   glClearColor(0.f, 0.f, 0.f, 1.f);
 
   glutSwapBuffers();
+  glutPostRedisplay();
 }
 
 int main(int argc, char *argv[]) {
   glutInit(&argc, argv);
 
+  glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
   const int win_x = 256;
   const int win_y = 256;
   glutInitWindowSize(win_x, win_y);
 
-  glutCreateWindow("gl");
   glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+  int window_ret = glutCreateWindow("gl");
+  if(window_ret < 1) std::exit(EXIT_FAILURE);
 
   glutKeyboardUpFunc(handleKeyboard);
   glutMouseFunc(handleMouse);
