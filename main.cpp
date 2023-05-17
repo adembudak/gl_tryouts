8446c2758f2b2212dc7223475b8fe4353db93ae4
adem <adembudak@outlook.com>
2023-05-17 17:46:03 +0300

    Walking through chapter 2

--- main.cpp
+++ main.cpp
@@ -24,9 +24,33 @@ void handleReshape(int new_pos_w, int new_pos_h) { //
 
 int frameCount = 0;
 
+GLuint VertexShaderId, FragmentShaderId, ProgramId;
+GLuint VaoId, VboId, ColorBufferId;
+
+const GLchar *const VertexShader =
+    R"(#version 400"
+     "layout(location=0) in vec4 in_Position;"
+     "layout(location=1) in vec4 in_Color;"
+     "out vec4 ex_Color;"
+
+     "void main(void) {"
+     "  gl_Position = in_Position;"
+     "  ex_Color = in_Color;"
+     "})";
+
+const GLchar *const FragmentShader =
+    R"(#version 400"
+    "in  vec4 ex_Color;"
+    "out vec4 out_Color;"
+
+    "void main(void) {"
+    "  out_Color = ex_Color;"
+    "})";
+
 void handleRendering() {
   ++frameCount;
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
 
   glClearColor(1.f, 0.f, 1.f, 1.f);
 
@@ -47,6 +71,88 @@ void timerFunction(int t) {
 
 void idleFunction() {
   glutPostRedisplay();
+}
+
+void createShaders() {
+  VertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(VertexShaderId, 1, &VertexShader, NULL);
+  glCompileShader(VertexShaderId);
+
+  FragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(FragmentShaderId, 1, &FragmentShader, NULL);
+  glCompileShader(FragmentShaderId);
+
+  ProgramId = glCreateProgram();
+  glAttachShader(ProgramId, VertexShaderId);
+  glAttachShader(ProgramId, FragmentShaderId);
+  glLinkProgram(ProgramId);
+  glUseProgram(ProgramId);
+
+  if(glGetError() != GL_NO_ERROR) std::exit(EXIT_FAILURE);
+}
+
+void destroyShaders() {
+  glUseProgram(0);
+
+  glDetachShader(ProgramId, VertexShaderId);
+  glDetachShader(ProgramId, FragmentShaderId);
+
+  glDeleteShader(FragmentShaderId);
+  glDeleteShader(VertexShaderId);
+
+  glDeleteProgram(ProgramId);
+
+  if(glGetError() != GL_NO_ERROR) std::exit(EXIT_FAILURE);
+}
+
+void createVBO() {
+  GLfloat Vertices[] = {
+      -0.8f, -0.8f, 0.0f, 1.0f, //
+      0.0f,  0.8f,  0.0f, 1.0f, //
+      0.8f,  -0.8f, 0.0f, 1.0f  //
+  };
+
+  GLfloat Colors[] = {
+      1.0f, 0.0f, 0.0f, 1.0f, //
+      0.0f, 1.0f, 0.0f, 1.0f, //
+      0.0f, 0.0f, 1.0f, 1.0f  //
+  };
+
+  glGenVertexArrays(1, &VaoId);
+  glBindVertexArray(VaoId);
+
+  glGenBuffers(1, &VboId);
+  glBindBuffer(GL_ARRAY_BUFFER, VboId);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+
+  glGenBuffers(1, &ColorBufferId);
+  glBindBuffer(GL_ARRAY_BUFFER, ColorBufferId);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Colors), Colors, GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(1);
+
+  if(glGetError() != GL_NO_ERROR) std::exit(EXIT_FAILURE);
+}
+
+void destroyVBO() {
+  glDisableVertexAttribArray(1);
+  glDisableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glDeleteBuffers(1, &ColorBufferId);
+  glDeleteBuffers(1, &VboId);
+
+  glBindVertexArray(0);
+  glDeleteVertexArrays(1, &VaoId);
+
+  if(glGetError() != GL_NO_ERROR) std::exit(EXIT_FAILURE);
+}
+
+void cleanUp() {
+  destroyVBO();
+  destroyShaders();
 }
 
 int main(int argc, char *argv[]) {
@@ -73,6 +179,7 @@ int main(int argc, char *argv[]) {
   glutDisplayFunc(handleRendering);
   glutIdleFunc(idleFunction);
   glutTimerFunc(0, timerFunction, 0);
+  glutCloseFunc(cleanUp);
 
   glutMainLoop();
 }
