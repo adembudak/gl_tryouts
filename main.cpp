536b8a5aa2b3958bdca7e7f6a9988d9e9f3e5c7d
Adem Budak <adembudak@outlook.com>
2025-01-17 18:35:19 +0300

    Set debug callback
    Copy-pasted from: https://www.khronos.org/opengl/wiki/Debug_Output#Examples

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <cstdio>
+
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
+                                GLenum severity, GLsizei length,
+                                const GLchar *message, const void *userParam) {
+  fprintf(stderr,
+          "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+          (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
+          message);
+}
 
 int main() {
   if (glfwInit() != GLFW_TRUE)
@@ -25,6 +35,8 @@ int main() {
   if (glewInit() != GLEW_OK)
     return 3;
 
+  glEnable(GL_DEBUG_OUTPUT);
+  glDebugMessageCallback(MessageCallback, 0);
 
 
   }
