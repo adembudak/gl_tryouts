b7ca578c894d7a44b130d1de1d24dafa2ed7c32b
Adem Budak <adembudak@outlook.com>
2025-01-17 18:33:17 +0300

    Removed code for print out GL version and extension related things

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <ranges>
 
 int main() {
   if (glfwInit() != GLFW_TRUE)
@@ -27,19 +25,8 @@ int main() {
   if (glewInit() != GLEW_OK)
     return 3;
 
-  const GLubyte *gl_version = glGetString(GL_VERSION);
-  std::cout << gl_version << '\n';
 
-  GLint version[2];
-  glGetIntegerv(GL_MAJOR_VERSION, &version[0]);
-  glGetIntegerv(GL_MINOR_VERSION, &version[1]);
-  std::cout << version[0] << '.' << version[1] << '\n';
 
-  GLint numExtensions;
-  glGetIntegerv(GL_NUM_EXTENSIONS, &numExtensions);
-  std::cout << numExtensions << '\n';
-  for (GLuint i : std::views::iota(0, numExtensions)) {
-    std::cout << glGetStringi(GL_EXTENSIONS, i) << '\n';
   }
 
   glClearColor(0.43, 0.109, 0.203, 1.0); // Claret violet
