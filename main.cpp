b05f3aaf17a20b36f8548b4e03960d450ed5b7b5
Adem Budak <adembudak@outlook.com>
2025-01-17 19:00:40 +0300

    Modified MessageCallback

--- main.cpp
+++ main.cpp
@@ -3,13 +3,53 @@
 
 #include <cstdio>
 
-void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id,
-                                GLenum severity, GLsizei length,
+#include <iostream>
+#include <sstream> // for std::ostringstream
+#include <utility> // for std::unreacheble
+
+void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
                                 const GLchar *message, const void *userParam) {
-  fprintf(stderr,
-          "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
-          (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : ""), type, severity,
-          message);
+
+  std::ostringstream sout;
+  sout << '\n';
+
+  switch(sout << "Source: "; source) {
+  case GL_DEBUG_SOURCE_API:             sout << "API"; break;
+  case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   sout << "Window system"; break;
+  case GL_DEBUG_SOURCE_SHADER_COMPILER: sout << "Shader compiler"; break;
+  case GL_DEBUG_SOURCE_THIRD_PARTY:     sout << "Third party"; break;
+  case GL_DEBUG_SOURCE_APPLICATION:     sout << "Application"; break;
+  case GL_DEBUG_SOURCE_OTHER:           sout << "Other"; break;
+  default:                              std::unreachable(); break;
+  }
+  sout << '\n';
+
+  switch(sout << "Type: "; type) {
+  case GL_DEBUG_TYPE_ERROR:               sout << "Error"; break;
+  case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: sout << "Deprecated behavior"; break;
+  case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  sout << "Undefined behavior"; break;
+  case GL_DEBUG_TYPE_PORTABILITY:         sout << "Portability"; break;
+  case GL_DEBUG_TYPE_PERFORMANCE:         sout << "Performance"; break;
+  case GL_DEBUG_TYPE_MARKER:              sout << "Marker"; break;
+  case GL_DEBUG_TYPE_PUSH_GROUP:          sout << "Push group"; break;
+  case GL_DEBUG_TYPE_POP_GROUP:           sout << "Pop group"; break;
+  case GL_DEBUG_TYPE_OTHER:               sout << "Other"; break;
+  default:                                std::unreachable(); break;
+  }
+  sout << '\n';
+
+  switch(sout << "Severity: "; severity) {
+  case GL_DEBUG_SEVERITY_LOW:          sout << "Low"; break;
+  case GL_DEBUG_SEVERITY_MEDIUM:       sout << "Medium"; break;
+  case GL_DEBUG_SEVERITY_HIGH:         sout << "High"; break;
+  case GL_DEBUG_SEVERITY_NOTIFICATION: sout << "Notification"; break;
+  default:                             std::unreachable(); break;
+  }
+  sout << '\n';
+
+  sout << "Message: " << message;
+
+  std::cout << sout.str();
 }
 
 int main() {
