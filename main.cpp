282fae3e80a616571c9c45689f32f4fd158bab7b
Adem Budak <adembudak@outlook.com>
2025-01-23 01:24:57 +0300

    Moved shaderLoader class to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,94 @@
-#include "shaderLoader.h"
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <range/v3/all.hpp>
+#include <mpark/patterns.hpp>
 
 #include <iostream>
 #include <sstream> // for std::ostringstream
 #include <utility> // for std::unreacheble
 #include <vector>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+namespace util {
+
+struct shaderLoader {
+  std::vector<GLuint> shaderIDs;
+
+  GLuint programID;
+
+  shaderLoader& load(const std::vector<std::filesystem::path>& shaderFiles);
+  shaderLoader& compile();
+  shaderLoader& attach();
+  shaderLoader& link();
+
+  GLuint getProgramID() const {
+    return programID;
+  }
+
+private:
+  GLenum identifyShaderType(const std::filesystem::path shaderFile) const;
+};
+
+shaderLoader& shaderLoader::load(const std::vector<std::filesystem::path>& shaderFiles) {
+  for(auto shaderFile : shaderFiles) {
+    std::ifstream fin{shaderFile};
+    fin.unsetf(std::ifstream::skipws);
+
+    std::string shaderSource = ranges::istream<char>(fin) | ranges::to<std::string>;
+
+    auto data = std::data(shaderSource);
+    auto size = std::size(shaderSource);
+
+    GLenum type = this->identifyShaderType(shaderFile);
+    GLuint id = glCreateShader(type);
+    glShaderSource(id, 1, &data, nullptr);
+
+    shaderIDs.push_back(id);
+  }
+
+  return *this;
+}
+
+shaderLoader& shaderLoader::compile() {
+  for(auto id : shaderIDs)
+    glCompileShader(id);
+
+  return *this;
+}
+
+shaderLoader& shaderLoader::attach() {
+  programID = glCreateProgram();
+
+  for(auto shaderID : shaderIDs)
+    glAttachShader(programID, shaderID);
+
+  return *this;
+}
+
+shaderLoader& shaderLoader::link() {
+  glLinkProgram(programID);
+
+  return *this;
+}
+
+GLenum shaderLoader::identifyShaderType(const std::filesystem::path shaderFile) const {
+  using namespace mpark::patterns;
+  // Based on: https://github.com/KhronosGroup/glslang?tab=readme-ov-file#execution-of-standalone-wrapper
+  // clang-format off
+  return match(shaderFile.extension().string())(
+      pattern(".vert") = [] { return GL_VERTEX_SHADER; },         
+      pattern(".tesc") = [] { return GL_TESS_CONTROL_SHADER; },   
+      pattern(".tese") = [] { return GL_TESS_EVALUATION_SHADER; },
+      pattern(".geom") = [] { return GL_GEOMETRY_SHADER; },       
+      pattern(".frag") = [] { return GL_FRAGMENT_SHADER; },       
+      pattern(".comp") = [] { return GL_COMPUTE_SHADER; }         
+  );
+  // clang-format on
+}
+
+}
 
 void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
                                 const GLchar* message, const void* userParam) {
@@ -96,6 +178,7 @@ int main() {
     return 3;
 
   glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_BLEND);
   glDebugMessageCallback(MessageCallback, 0);
 
   GLuint programID = util::shaderLoader{}
