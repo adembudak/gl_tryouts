e90c3939f8e926b8a14c050d3df09d5edabc73f9
Adem Budak <adembudak@outlook.com>
2025-02-23 18:58:11 +0300

    Leaky abstractions, buggy file format implementations, bad OOP

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <range/v3/view/istream.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/transform.hpp>
+#include <range/v3/algorithm/equal.hpp>
 #include <range/v3/algorithm/find.hpp>
 #include <mpark/patterns/match.hpp>
 
@@ -24,8 +25,236 @@
 #include <string>
 #include <cassert>
 #include <numbers>
+#include <cstdint>
+#include <iterator>
 
 namespace util {
+
+namespace ktx { namespace v1_0 {
+// KTX file format loader:
+// Rip off from: https://github.com/openglsuperbible/sb7code/blob/master/src/sb7/sb7ktx.cpp
+// Spec: https://registry.khronos.org/KTX/specs/1.0/ktxspec.v1.html
+// Up to date spec: https://registry.khronos.org/KTX/specs/2.0/ktxspec.v2.html
+
+using ubyte = std::uint8_t;
+using uint32 = std::uint32_t;
+
+struct Header_t {
+  ubyte identifier[12];
+  uint32 endianness;
+  uint32 glType;
+  uint32 glTypeSize;
+  uint32 glFormat;
+  uint32 glInternalFormat;
+  uint32 glBaseInternalFormat;
+  uint32 pixelWidth;
+  uint32 pixelHeight;
+  uint32 pixelDepth;
+  uint32 nArrayElements;
+  uint32 nFaces;
+  uint32 nMipmapLevel;
+  uint32 bytesOfKeyValueData;
+};
+
+constexpr std::array<ubyte, 12> identifierExpected{0xAB, 0x4B, 0x54, 0x58, 0x20, 0x31, 0x31, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A};
+
+struct ktxLoader {
+  GLuint textureID;
+  std::vector<char> texels;
+
+  bool load(const std::filesystem::path& textureFile);
+
+private:
+  Header_t header;
+  GLenum target;
+
+  bool is_ktx() const;
+  uint32 calculate_stride(uint32 width, uint32 pad = 4) const;
+  uint32 calculate_face_size() const;
+};
+
+bool ktxLoader::is_ktx() const {
+  return ranges::equal(header.identifier, identifierExpected);
+}
+
+uint32 ktxLoader::calculate_stride(uint32 width, uint32 pad) const {
+  uint32 channels = 0;
+  switch(header.glBaseInternalFormat) {
+  case GL_RED:  channels = 1; break;
+  case GL_RG:   channels = 2; break;
+  case GL_BGR:
+  case GL_RGB:  channels = 3; break;
+  case GL_BGRA:
+  case GL_RGBA: channels = 4; break;
+  }
+
+  uint32 stride = header.glTypeSize * channels * width;
+
+  stride = (stride + (pad - 1)) & ~(pad - 1);
+
+  return stride;
+}
+
+uint32 ktxLoader::calculate_face_size() const {
+  return calculate_stride(header.pixelWidth) * header.pixelHeight;
+};
+
+bool ktxLoader::load(const std::filesystem::path& textureFile) {
+  std::ifstream fin{textureFile, std::ios::binary};
+
+  if(!fin)
+    return false;
+
+  fin.read(reinterpret_cast<char*>(&header), sizeof(decltype(header)));
+
+  if(!is_ktx())
+    return false;
+
+  auto swap32 = [](const uint32 u32) -> uint32 {
+    union {
+      uint32 u32;
+      ubyte u8[4];
+    } a, b;
+
+    a.u32 = u32;
+    b.u8[0] = a.u8[3];
+    b.u8[1] = a.u8[2];
+    b.u8[2] = a.u8[1];
+    b.u8[3] = a.u8[0];
+
+    return b.u32;
+  };
+
+  if(header.endianness == 0x01020304) {
+    header.endianness = swap32(header.endianness);
+    header.glType = swap32(header.glType);
+    header.glTypeSize = swap32(header.glTypeSize);
+    header.glFormat = swap32(header.glFormat);
+    header.glInternalFormat = swap32(header.glInternalFormat);
+    header.glBaseInternalFormat = swap32(header.glBaseInternalFormat);
+    header.pixelWidth = swap32(header.pixelWidth);
+    header.pixelHeight = swap32(header.pixelHeight);
+    header.pixelDepth = swap32(header.pixelDepth);
+    header.nArrayElements = swap32(header.nArrayElements);
+    header.nFaces = swap32(header.nFaces);
+    header.nMipmapLevel = swap32(header.nMipmapLevel);
+    header.bytesOfKeyValueData = swap32(header.bytesOfKeyValueData);
+  } else {
+    assert(header.endianness == 0x04030201);
+  }
+
+  target = GL_NONE;
+
+  if(header.pixelHeight == 0) {
+    target = (header.nArrayElements == 0) ? GL_TEXTURE_1D : GL_TEXTURE_1D_ARRAY;
+  } else if(header.pixelDepth == 0) {
+    if(header.nArrayElements == 0)
+      target = (header.nFaces == 6) ? GL_TEXTURE_CUBE_MAP : GL_TEXTURE_2D;
+    else
+      target = (header.nFaces == 6) ? GL_TEXTURE_CUBE_MAP_ARRAY : GL_TEXTURE_2D_ARRAY;
+  } else {
+    target = GL_TEXTURE_3D;
+  }
+
+  assert(target != GL_NONE);
+
+  glCreateTextures(target, 1, &textureID);
+
+  std::istreambuf_iterator<char> iter{fin};
+  texels = std::vector<char>(next(iter, header.bytesOfKeyValueData), {});
+
+  const auto& h = header;
+  switch(target) {
+  case GL_TEXTURE_1D:
+    glTextureStorage1D(textureID, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth);
+    glTextureSubImage1D(textureID, h.nMipmapLevel, 0, h.pixelWidth, h.glFormat, h.glType, std::data(texels));
+    break;
+
+  case GL_TEXTURE_1D_ARRAY:
+    glTextureStorage2D(textureID, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight);
+
+    glTextureSubImage2D(textureID, h.nMipmapLevel, 0, 0, h.pixelWidth, h.pixelHeight, h.glFormat, h.glType,
+                        std::data(texels));
+
+    break;
+
+  case GL_TEXTURE_2D:
+    if(h.glType == GL_NONE)
+      glCompressedTexImage2D(GL_TEXTURE_2D, 0, h.glInternalFormat, h.pixelWidth, h.pixelHeight, 0, std::size(texels),
+                             std::data(texels));
+
+    else {
+      glTexStorage2D(textureID, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight);
+
+      uint32 pixelWidth = h.pixelWidth;
+      uint32 pixelHeight = h.pixelHeight;
+
+      auto ptr = std::data(texels);
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+      for(int i = 0; i < h.nMipmapLevel; i++) {
+        glTexSubImage2D(GL_TEXTURE_2D, i, 0, 0, pixelWidth, pixelHeight, h.glFormat, h.glType, ptr);
+
+        ptr += h.pixelHeight * calculate_stride(h.pixelWidth, 1);
+
+        pixelHeight >>= 1;
+        pixelWidth >>= 1;
+
+        if(!pixelHeight)
+          pixelHeight = 1;
+        if(!pixelWidth)
+          pixelWidth = 1;
+      }
+    }
+    break;
+
+  case GL_TEXTURE_2D_ARRAY:
+    glTexStorage3D(GL_TEXTURE_2D_ARRAY, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight, h.nArrayElements);
+    glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, 0, h.pixelWidth, h.pixelHeight, h.nArrayElements, h.glFormat, h.glType,
+                    std::data(texels));
+
+    break;
+
+  case GL_TEXTURE_CUBE_MAP:
+    glTexStorage2D(GL_TEXTURE_CUBE_MAP, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight);
+    glTexSubImage3D(GL_TEXTURE_CUBE_MAP, 0, 0, 0, 0, h.pixelWidth, h.pixelHeight, h.nFaces, h.glFormat, h.glType,
+                    std::data(texels));
+    {
+      int face_size = calculate_face_size();
+      for(int i = 0; i < h.nFaces; i++) {
+        glTexSubImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, 0, 0, h.pixelWidth, h.pixelHeight, h.glFormat, h.glType,
+                        std::data(texels) + face_size * i);
+      }
+    }
+    break;
+  case GL_TEXTURE_CUBE_MAP_ARRAY:
+    glTexStorage3D(GL_TEXTURE_CUBE_MAP_ARRAY, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight,
+                   h.nArrayElements);
+    glTexSubImage3D(GL_TEXTURE_CUBE_MAP_ARRAY, 0, 0, 0, 0, h.pixelWidth, h.pixelHeight, h.nFaces * h.nArrayElements,
+                    h.glFormat, h.glType, std::data(texels));
+    break;
+
+  case GL_TEXTURE_3D:
+    glTexStorage3D(GL_TEXTURE_3D, h.nMipmapLevel, h.glInternalFormat, h.pixelWidth, h.pixelHeight, h.pixelDepth);
+    glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, 0, h.pixelWidth, h.pixelHeight, h.pixelDepth, h.glFormat, h.glType,
+                    std::data(texels));
+    break;
+  };
+
+  return true;
+}
+
+}}
+
+struct textureLoader {
+  GLuint textureID;
+  std::vector<glm::vec4> texels;
+
+  bool load(const std::filesystem::path& textureFile);
+
+  GLuint getTextureID() const {
+    return textureID;
+  }
+};
 
 struct shaderLoader {
   std::vector<GLuint> shaderIDs;
@@ -182,10 +411,76 @@ void GLAPIENTRY MessageCallback(GLenum source, GLenum type, GLuint id, GLenum se
   std::cout << sout.str();
 }
 
-glm::vec3 eye(0.0, 0.0, 2.5);
-glm::vec3 center(0.0, 0.0, 0.0);
-glm::vec3 up(0.0, 1.0, 0.0);
-glm::mat4x4 view = glm::lookAt(eye, center, up);
+///
+
+struct Camera {
+  glm::vec3 eye{0.0, 0.0, 2.5};    // Gaze direction
+  glm::vec3 center{0.0, 0.0, 0.0}; // where to look point camera
+  glm::vec3 up{0.0, 1.0, 0.0};     // camera orientation
+
+public:
+  glm::mat4x4 update(double t) {
+    return glm::lookAt(eye, center, up);
+  }
+};
+
+Camera cam;
+
+struct Model {
+  std::vector<glm::vec3> vertexPositions;
+  std::vector<GLuint> indices;
+  std::vector<glm::vec3> texturePositons;
+  std::vector<glm::vec3> colors;
+
+  GLuint vertexArrayID;
+
+  // void load(std::filesystem::path& modelFile);
+  Model& load();
+  GLuint getVertexArrayID() const {
+    return vertexArrayID;
+  }
+
+private:
+  Model& loadPositionVertices();
+  Model& loadIndices();
+};
+
+Model& Model::load() {
+  glCreateVertexArrays(1, &vertexArrayID);
+  glBindVertexArray(vertexArrayID);
+
+  loadPositionVertices().loadIndices();
+
+  return *this;
+}
+
+Model& Model::loadPositionVertices() {
+  GLuint arrayBufferID;
+  glCreateBuffers(1, &arrayBufferID);
+  glBindBuffer(GL_ARRAY_BUFFER, arrayBufferID);
+
+  GLsizeiptr sizeOfVertex = sizeof(decltype(vertexPositions[0]));
+  GLsizeiptr sizeOfVertices = std::size(vertexPositions) * sizeOfVertex;
+  glNamedBufferStorage(arrayBufferID, sizeOfVertices, nullptr, GL_DYNAMIC_STORAGE_BIT);
+  glNamedBufferSubData(arrayBufferID, 0, sizeOfVertices, std::data(vertexPositions));
+
+  glVertexAttribPointer(0, vertexPositions[0].length(), GL_FLOAT, GL_FALSE, sizeOfVertex, nullptr);
+  glEnableVertexAttribArray(0);
+
+  return *this;
+}
+
+Model& Model::loadIndices() {
+  GLuint elementBufferID;
+  glCreateBuffers(1, &elementBufferID);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferID);
+
+  GLsizeiptr sizeOfIndex = sizeof(decltype(indices[0]));
+  GLsizeiptr sizeOfIndices = std::size(indices) * sizeOfIndex; // in bytes
+  glNamedBufferStorage(elementBufferID, sizeOfIndices, std::data(indices), GL_MAP_READ_BIT);
+
+  return *this;
+}
 
 constexpr auto pi = std::numbers::pi_v<float>;
 
@@ -257,12 +552,10 @@ void keyInput_callback(GLFWwindow* window, int key, int scancode, int action, in
 
 void mouseScroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
   switch(int(yoffset)) {
-  case 1:  eye.y += 1.0; break;
-  case -1: eye.y -= 1.0; break;
+  case 1:  cam.eye.y += 1.0; break;
+  case -1: cam.eye.y -= 1.0; break;
   default: break;
   }
-
-  view = glm::lookAt(eye, center, up);
 }
 
 void errorCallback(int error, const char* description) {
@@ -345,29 +638,10 @@ int main() {
 
   const std::vector<GLuint> indices = {0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7, 0, 3, 7, 0, 7, 4,
                                        1, 2, 6, 1, 6, 5, 0, 1, 5, 0, 5, 4, 3, 2, 6, 3, 6, 7};
-
-  GLuint vertexArrayObject;
-  glCreateVertexArrays(1, &vertexArrayObject);
-  glBindVertexArray(vertexArrayObject);
-
-  GLuint arrayBufferID;
-  glCreateBuffers(1, &arrayBufferID);
-  glBindBuffer(GL_ARRAY_BUFFER, arrayBufferID);
-
-  GLuint elementBufferID;
-  glCreateBuffers(1, &elementBufferID);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementBufferID);
-
-  GLsizeiptr sizeOfIndices = std::size(indices) * sizeof(decltype(indices[0])); // in bytes
-  glNamedBufferStorage(elementBufferID, sizeOfIndices, std::data(indices), GL_MAP_READ_BIT);
-
-  GLsizeiptr sizeOfVertices = std::size(vertexData) * sizeof(Vertex);
-
-  glNamedBufferStorage(arrayBufferID, sizeOfVertices, nullptr, GL_DYNAMIC_STORAGE_BIT);
-
-  glNamedBufferSubData(arrayBufferID, 0, sizeOfVertices, std::data(vertexData));
-  glVertexAttribPointer(0, vertexData[0].length(), GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-  glEnableVertexAttribArray(0);
+  Model cube;
+  cube.vertexPositions = std::move(vertexData);
+  cube.indices = std::move(indices);
+  GLuint vertexArrayID = cube.load().getVertexArrayID();
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -380,9 +654,9 @@ int main() {
     glClearBufferfv(GL_COLOR, 0, backgroundColor);
 
     glUniformMatrix4fv(tranformMatrixLocation, 1, GL_FALSE, glm::value_ptr(transform));
-    glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(cam.update(0.0)));
     glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
-    glBindVertexArray(vertexArrayObject);
+    glBindVertexArray(vertexArrayID);
 
     glDrawElements(GL_TRIANGLES, std::size(indices), GL_UNSIGNED_INT, nullptr);
 
