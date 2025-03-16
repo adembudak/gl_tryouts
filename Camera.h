ac07b2d589b25b8e3103b4667240a9960c68ce09
Adem Budak <adembudak@outlook.com>
2025-03-16 16:18:47 +0300

    Header clean up

--- Camera.h
+++ Camera.h
@@ -1,10 +1,6 @@
 #pragma once
 
-#include <GL/glew.h>
-
-#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
-#include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtc/constants.hpp>
